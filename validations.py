ee6a1b80a777880c9cd351f0d560bb591c763c7c
Bahri Aya <aya.bahri@insat.u-carthage.tn>
2020-04-14 03:33:29 +0100

    Update validations.py

--- validations.py
+++ validations.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
-
-def vailidate_user(username, minlen):
-    """Checks if the recieved username matches the required conditions."""
+def validate_user(username, minlen):
+    """Checks if the received username matches the required conditions."""
     if type(username) != str:
+        print("not a string")
         raise TypeError("username must be a string")
     if minlen < 1:
-        raise ValueError("minlen must be at least 1".)
-        
-        if len(username) < minlen:
-            return False
-        if not username.isalnum():
-            return False
-        # Usernames can't begin with a number
-        if username [0].isnumeric():
-            return False
-        return True
+        print("minlen is too short")
+        raise ValueError("minlen must be at least 1.")
+    if len(username) < minlen:
+        print("too short my dear..")
+        return False
+    if not username.isalnum():
+        print("bad choice of characters!")
+        return False
+    # Usernames can't begin with a number
+    if username [0].isnumeric():
+        print("You can't start with a number.")
+        return False
+    return True
+
+def main():
+    minlen = 3;
+    username = input("Enter username \n")
+    if validate_user(username,minlen):
+        print("Welcome", username)
+    pass
+
+main()
